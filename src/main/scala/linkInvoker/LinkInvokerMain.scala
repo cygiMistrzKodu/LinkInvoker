877dd065a94b769811c83fab2311e70a682af61b
JacekMistrzuKoder <jacek301@gmail.com>
2017-12-21 02:11:13 +0100

    Here Is Wird Thing How to Handel Event Listener with parameter in Scala FX 2 Method Show Take a Look if you forget. In This Case is Handling in TextField control event  text.onChange when text changes in TextField is fired

--- src/main/scala/linkInvoker/LinkInvokerMain.scala
+++ src/main/scala/linkInvoker/LinkInvokerMain.scala
@@ -14,48 +14,198 @@ import scalafx.scene.control.Button
 import scalafx.scene.layout.VBox
 import scalafx.geometry.Pos
 import scalafx.scene.layout.HBox
+import scalafx.scene.text.Font
+import scala.io.Position
+import scalafx.scene.text.TextAlignment
+import scalafx.event.ActionEvent
+import scalafx.Includes._
+import javafx.beans.value.ChangeListener
+import scalafx.beans.value.ObservableValue
 
-object LinkInvokerMain {//extends JFXApp {
-      def  main(args: Array[String]): Unit = {
+object LinkInvokerMain extends JFXApp {
+  //      def  main(args: Array[String]): Unit = {
 
-      val linkCreator = new LinkCreator("mr Robot",1,4, 1, 10)
-      val links = linkCreator.getLinks
+  //      val linkCreator = new LinkCreator("mr Robot",1,4, 1, 10)
+  //      val links = linkCreator.getLinks
 
-         
-//     Gui.main(Array())
+  //     Gui.main(Array())
 
   //    a.main(args: _ *)
 
   //    new LinkSender(links)
-      
-      
-      for (link <- links) println(link)
+
+  //      for (link <- links) println(link)
+
+  //    }
+
+  stage = new PrimaryStage() {
+
+    title = "Link Invoker"
+
+    val seasonRangeChooserHbox = new HBox {
+
+      spacing = 6
+      alignment = Pos.Center
+
+      val fromSeasonLabel = new Label("From: ") {
+        font = Font.font(15)
+      }
+      val beginSeasonNumberInput = new TextField {
+        text = deafultNumber
+        prefWidth = getInputPrefSize
+        alignment = textAligment
+      }
+
+      val toSeasonLabel = new Label("To: ") {
+        font = Font.font(15)
+      }
+      val endSeasonNumberInput = new TextField {
+        text = deafultNumber
+        prefWidth = getInputPrefSize
+        alignment = textAligment
+      }
+
+      children = Seq(fromSeasonLabel, beginSeasonNumberInput, toSeasonLabel, endSeasonNumberInput)
 
     }
 
-//  stage = new PrimaryStage() {
+    val seasonChooser = new VBox {
+
+      spacing = 6
+      alignment = Pos.Center
+
+      val seasonName = new Label("Seasons choose") {
+        font = Font.font(18)
+      }
+
+      children = Seq(seasonName, seasonRangeChooserHbox)
+    }
+
+    val episodeRangeChooserHbox = new HBox {
+
+      spacing = 6
+      alignment = Pos.Center
+
+      val fromEpisodeLabel = new Label("From: ") {
+        font = Font.font(15)
+      }
+      val beginEpisodeNumberInput = new TextField {
+        text = deafultNumber
+        prefWidth = getInputPrefSize
+        alignment = textAligment
+        text.onChange {
+          
+          (o: ObservableValue[_ <: String , _ <: String], oldVal: String, newVal: String) => {  //First Solution to Handel WIRD THING With Parameters
+            
+             println("Mamusika")
+             
+              println("new value:" + newVal)
+        
+               println("old value:" + oldVal)
+            
+          }
+           
+//          def dupa(ab: ObservableValue[String, String], a: String, b: String): Unit = {
+//            println("DUpa DUp Dupa dupa")
 //
-//    val vBox = new HBox {
-//
-//      spacing = 6
-//
-//      alignment = Pos.Center
-//
-//      children = Seq(new Button("lalalala"), new Button("pasternak"))
-//      
-//     
-//
-//    }
-//
-//    scene = new Scene(300, 300) {
-//
-//      root = new StackPane {
-//
-//        children = Seq(vBox)
-//
-//      }
-//
-//    }
+//          }
+
+          //          if (text.get.isEmpty()){
+          //             text = deafultNumber
+          //          }
+          
+           
+          
+//           println("lala")
+
+        }
+
+      }
+      
+      
+      beginEpisodeNumberInput.textProperty().addListener((observable,oldValue,newValue) => { // Second Solution to Handel Event WIth Parameters
+        
+//        println("new value:" + oldValue)
+//        
+//        println("old value:" + newValue)
+        
+      })
+      
+      
+      
+
+      val toEpisodeLabel = new Label("To: ") {
+        font = Font.font(15)
+      }
+      val endEpisodeNumberInput = new TextField {
+        text = deafultNumber
+        prefWidth = getInputPrefSize
+        alignment = textAligment
+      }
+
+      val seasonName = new Label("Seasons choose")
+
+      children = Seq(fromEpisodeLabel, beginEpisodeNumberInput, toEpisodeLabel, endEpisodeNumberInput)
+
+    }
+
+    val episdoeChooser = new VBox {
+
+      spacing = 6
+      alignment = Pos.Center
+
+      val episdoeName = new Label("Episode choose") {
+        font = Font.font(18)
+      }
+
+      children = Seq(episdoeName, episodeRangeChooserHbox)
+    }
+
+    val rangeChooser = new VBox {
+
+      spacing = 6
+      alignment = Pos.Center
+
+      val serialNameLabel = new Label("Input Movie Name") {
+        font = Font.font(20)
+
+      }
+      val movieNameInput = new TextField {
+        spacing = 10
+        alignment = textAligment
+      }
+
+      val sendRequestButton = new Button("Send") {
+        prefWidth = 100
+        prefHeight = 50
+        font = Font.font(20)
+      }
+
+      children = Seq(serialNameLabel, movieNameInput, seasonChooser, episdoeChooser, sendRequestButton)
+    }
+
+    scene = new Scene(460, 280) {
+
+      resizable = false
+
+      root = new StackPane {
+
+        children = Seq(rangeChooser)
+
+      }
+
+    }
+
+  }
+
+  def getInputPrefSize: Double = 120
+
+  def textAligment: Pos = Pos.CENTER
+
+  def deafultNumber: String = "1"
+
+//  def dupa(ab: ObservableValue[String, String], a: String, b: String): Unit = {
+//    println("DUpa DUp Dupa dupa")
 //
 //  }
 
